b3359ca6e3cf68a3e966b206dc018c7cf49cdf56
PedroGB229 <pedrogoncalvesbrito0@gmail.com>
2025-12-05 20:13:09 -0300

    C

---
+++ html/js/fornecedor.js
@@ -0,0 +1,22 @@
+
+import { Requests } from "./Requests.js";
+import { Validate } from "./Validate.js";
+
+const InsertButton = document.getElementById('insert');
+
+$('#cpf_cnpj').inputmask({ "mask": ["999.999.999-99", "99.999.999/9999-99"] });
+
+InsertButton.addEventListener('click', async () => {
+    /*const IsValid = Validate
+        .SetForm('form')
+        .Validate();
+    console.log(IsValid);*/
+    const response = await Requests.SetForm('form').Post('/fornecedor/insert');
+});
+/*
+const Salvar = document.getElementById('salvar');
+
+Salvar.addEventListener('click', async () => {
+    Validate.SetForm('form').Validate();
+    console.log(response);
+});*/
